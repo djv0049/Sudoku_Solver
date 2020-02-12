fd0a871c571b99e3ee936e0dcf1b91353bdaf892
djv0049 <44690969+djv0049@users.noreply.github.com>
2020-02-12 16:17:14 +1300

    Update Gui.java
    properly displays the sudoku as it is being etered

--- SudokuSolver/src/Gui.java
+++ SudokuSolver/src/Gui.java
@@ -44,26 +44,62 @@ public class Gui extends JFrame{
 		// add event listeners.
 		ActionListener myListener = new MyActionListener();
 		addBtn.addActionListener(myListener);
+		inputTxt.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				addLineToDisplay();				
+			}
+		});		
 		
 	}
-	
+	/*
+	 * need to change how all this works so that it'll work on other labels. not just the solution label
+	 */
 	public void printSudokuLine(String row) {
 		String[] s = row.split("(?<=\\G...)");
-		String result = "<html>";
+		String result = "";
+		if(solution.getText().length() == 0 ) {
+			result = "<html>";
+		}
 		for(String i : s) {
 			result += i;
-			result += " ";
+			result += "  ";
 		}
 		result +="<br/>";
 		result += "</html>";
-		solution.setText(solution.getText().concat(result));
-		int length = solution.getText().length();
-		if(length == 36 || length == 73) {
-			solution.setText(solution.getText().concat("\n"));
+		System.out.print(solution.getText().length());
+		if(solution.getText().length() > 0) {
+			result = solution.getText().replaceAll("</html>",result);
+		}
+		System.out.print(result);
+		int lineCount = result.split("<br/>").length;
+		if(lineCount == 4 || lineCount == 7) {
+			result = 
+					result.replaceAll("</html>", "  <br/></html>");
+		}
+		if(lineCount <= 11) {
+			solution.setText(result);
+			this.s.addFromInput(inputTxt.getText());
+			
+		}
+		else {
+			System.out.println("\nthere are already 9 lines, click solve");
 		}
 		
 	}
 	
+	public void addLineToDisplay() {
+	// send the line to a function that adds the line to the array of lines
+		if(inputTxt.getText().length() == 9) {
+			printSudokuLine(inputTxt.getText());
+			inputTxt.setText("");
+			
+		}
+		else {
+			System.out.print("please enter 9 digits without spaces");
+		}
+		System.out.println("this is displayed when you press the button");
+	}
 	// to do: 
 	// make an event handler 
 	// make a function to take the input from text box 
@@ -80,17 +116,7 @@ public class Gui extends JFrame{
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			if(e.getSource() == addBtn) {
-				// send the line to a function that adds the line to the array of lines
-				if(inputTxt.getText().length() == 9) {
-					s.addFromInput(inputTxt.getText());
-					printSudokuLine(inputTxt.getText());
-					inputTxt.setText("");
-					
-				}
-				else {
-					System.out.print("please enter 9 digits without spaces");
-				}
-				System.out.println("this is displayed when you press the button");
+				addLineToDisplay();
 			}
 		}
 	}
@@ -100,12 +126,16 @@ public class Gui extends JFrame{
 		
 		@Override
 		public void processKeyEvent(KeyEvent ev) {
+			if(KeyEvent.VK_ENTER == ev.getKeyCode()) {
+				addLineToDisplay();
+			}
 			if(Character.isDigit(ev.getKeyChar()) || ev.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
 				super.processKeyEvent(ev);
 			}
 			ev.consume();
 			return;	
 		}
+		
 	}
 }
 
